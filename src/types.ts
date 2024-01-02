338f442b728a79b244724a824b3dadb0846a399e
Doğuş Erdem <48646654+DogusErdem@users.noreply.github.com>
2024-01-02 16:21:32 +0300

    fix: dropdown onslect prop type edit

--- src/types.ts
+++ src/types.ts
@@ -145,7 +145,7 @@ export type DrowdownProps = {
     containerStyle?: StyleProp<ViewStyle>
     buttonTitle?: string,
     rowStyle?: StyleProp<ViewStyle>
-    onSelect: (item: Array<string> | Array<object> | object) => void,
+    onSelect: (item: Array<string> | Array<object> | object | string) => void,
     defaultValue?: Array<object | string>,
     rowTextStyle?: StyleProp<TextStyle>,
     buttonTextStyle?: StyleProp<TextStyle>,
