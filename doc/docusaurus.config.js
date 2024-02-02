becca43bac65ba25c9b453ca887781f1dfdceee0
Ahmet Kağan Yörük <ahmetkagan@hotmail.com>
2024-02-02 10:28:12 +0300

    Docusaurus edit url fixed

--- doc/docusaurus.config.js
+++ doc/docusaurus.config.js
@@ -10,8 +10,6 @@ const commonDocsOptions = {
   breadcrumbs: false,
   showLastUpdateAuthor: false,
   showLastUpdateTime: true,
-  editUrl:
-    'https://github.com/TRA-Tech/react-native-kitra/tree/main/',
 };
 
 /** @type {import('@docusaurus/types').Config} */
@@ -41,9 +39,8 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-
           editUrl:
-            'https://github.com/TRA-Tech/react-native-kitra/tree/main',
+            'https://github.com/TRA-Tech/react-native-kitra/tree/main/doc',
           ...commonDocsOptions,
 
         },
