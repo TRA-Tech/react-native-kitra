cdad529528d33f2390e4a3b6154be744d4ccaf9b
Ahmet Kağan Yörük <47483182+KaganYrk@users.noreply.github.com>
2024-03-06 15:13:41 +0300

    Reanimated jest utils path updated

--- testSetup.js
+++ testSetup.js
@@ -1,7 +1,6 @@
-import { setUpTests } from 'react-native-reanimated/lib/commonjs/reanimated2/jestUtils';
+require('react-native-reanimated/src/reanimated2/jestUtils').setUpTests();
 require('@shopify/flash-list/jestSetup');
 
-setUpTests();
 jest.mock('react-native-reanimated', () => require('react-native-reanimated/mock'));
 jest.mock('expo-linear-gradient', () => 'LinearGradient');
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
