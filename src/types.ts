cf2d2d6b5a7b924cc6dfbb74e1b6de1a33852817
Doğuş Erdem <48646654+DogusErdem@users.noreply.github.com>
2024-04-29 10:32:56 +0300

    fix: Slider component container style edited.

--- src/types.ts
+++ src/types.ts
@@ -568,6 +568,10 @@ export type SearchBarProps = {
 }
 
 export type SliderProps = {
+     /**
+        *Additional styles to apply to the slider container
+    */
+        containerStyle?: StyleProp<ViewStyle>,
     /**
         *Determines whether to show the percentage indicator above the slider button
      */
